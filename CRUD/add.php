10cffd4be73db819e3f0738b9c79d137b8fecac5
rajpatwa <75309916+rajpatwa@users.noreply.github.com>
2026-02-05 21:59:17 +0545

    Update add.php

--- CRUD/add.php
+++ CRUD/add.php
@@ -1,1 +1,25 @@
+<?php
+include "conn.php";
 
+if(isset($_POST['submit'])){
+      $fullname = $_POST['fullname'];
+      $gender = $_POST['gender'];
+      $age = $_POST['age'];
+      $address = $_POST['address'];
+
+  $sql = "INSERT into students (name,gender,age,address) VALUES ('$fullname','$gender','$age','$address')";
+    if($conn->query($sql)=== TRUE){
+        echo "Student added successfully!";
+    } else{
+      echo "Student not added.". $conn->error;
+    }
+}
+
+<html>
+  <head></head>
+  <body>
+    <form method = "POST" action ="">
+  Name:<input type = "name" name = "fullname"  placeholder ="Enter the Full Name."><br>
+  Gender:<input type = "" name = "gender"  placeholder ="Enter the gender.."><br>
+  </form>
+  </body>
